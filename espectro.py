48c7e5db2076eb2a8a6e8199e8169e85aa14f632
vquadrado <69911862+vquadrado@users.noreply.github.com>
2021-11-23 23:27:33 -0300

    Feito gráficos, falta escrever texto e conferir se ta certo

--- espectro.py
+++ espectro.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """ Rotina que exibe o espectro de magnitude (X(ejw)) de um sinal discreto """
-def espectro(y):
+def espectro(y,name="default"):
 
     #modulo da transf. de Fourier
     Y = np.abs(np.fft.fft(y))
@@ -13,10 +13,12 @@ def espectro(y):
     #exibe o grafico do espectro
     plt.figure() 
     plt.plot(w,Y/np.max(Y))
-    plt.xlabel('$\Omega$ [rad]', fontsize=10)
-    plt.ylabel('|$Y(e^{j\Omega})$|', fontsize=10)
+    plt.xlabel('$\Omega$ [rad]', fontsize=15)
+    plt.ylabel('|$Y(e^{j\Omega})$|', fontsize=15)
     plt.grid(True)
     plt.xlim((0,2*math.pi))
-    plt.show()
+
+    if name != "default":
+        plt.savefig(f'{name}.png')
     
     return Y    
